052491b5143cb13662baf3c084365854507d00ee
Priyanshu-Koley <priyanshukoley0@gmail.com>
2024-03-08 22:29:46 +0530

    Added Assignment_6_CS

---
+++ Assignment_6_CS/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Enter the URL:");
+        string url = Console.ReadLine();
+
+        try
+        {
+            await DownloadAndWriteToFileAsync(url);
+            Console.WriteLine("Content has been successfully written to A.txt.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+    }
+
+    static async Task DownloadAndWriteToFileAsync(string url)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            HttpResponseMessage response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode(); // Throws exception if not successful
+
+            using (Stream contentStream = await response.Content.ReadAsStreamAsync())
+            {
+                using (StreamReader reader = new StreamReader(contentStream))
+                {
+                    using (StreamWriter writer = new StreamWriter("A.txt"))
+                    {
+                        while (!reader.EndOfStream)
+                        {
+                            string line = await reader.ReadLineAsync();
+                            await writer.WriteLineAsync(line);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
